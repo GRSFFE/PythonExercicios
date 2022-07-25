2d4721f63c311b4548a3ac512d030d1707d61f2f
Germanno Resende <germannoresende@gmail.com>
2022-07-25 20:50:26 -0300

    Complemento de 4 exercicios.

---
+++ ex059.py
@@ -0,0 +1,34 @@
+from time import sleep
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opcao = 0
+while opcao != 5:
+    print('''    [ 1 ] Somar
+    [ 2 ] Multiplicar
+    [ 3 ] Maior
+    [ 4 ] Novos números
+    [ 5 ] Sair do programa''')
+    opcao = int(input('>>>>> Qual é a sua opção? '))
+    if opcao == 1:
+        soma = n1 + n2
+        print(f'A soma entre {n1} + {n2} é igual a {soma}')
+    elif opcao == 2:
+        multiplicacao = n1 * n2
+        print(f'A multiplicacao de {n1} x {n2} é igual a {multiplicacao}')
+    elif opcao == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print(f'Entre {n1} e {n2} o maior número é o {maior}')
+    elif opcao == 4:
+        print('Informe os valores novamente:')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opcao == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente.')
+    print('-=-' * 10)
+    sleep(2)
+print('Fim do programa! Volte sempre!')
---
+++ ex060.py
@@ -0,0 +1,6 @@
+from math import factorial
+from time import sleep
+n = int(input('Digite um número para calcular seu fatorial: '))
+f = factorial(n)
+print(f'O fatorial de {n} é {f}')
+sleep(2)
